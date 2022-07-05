2c3595f378f04e77ffd59be60f03f3dc83d43b90
Mouhcine <mouhcine.mesmouki7@gmail.com>
2022-07-05 02:46:12 +0100

    removed some unnecessary files

--- src/assets/theme/functions/gradientChartLine.js
+++ src/assets/theme/functions/gradientChartLine.js
@@ -1,19 +1,4 @@
 /**
-=========================================================
-* Sigma React - v2.1.0
-=========================================================
-
-* Product Page: https://www.github.com/crushoverride007/product/material-dashboard-react
-* Copyright 2022 Matrix-Nexus (https://www.github.com/crushoverride007)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-/**
   The gradientChartLine() function helps you to create a gradient color for the chart line
  */
 
--- src/assets/theme/theme-rtl.js
+++ src/assets/theme/theme-rtl.js
@@ -1,18 +1,3 @@
-/**
-=========================================================
-* Sigma React - v2.1.0
-=========================================================
-
-* Product Page: https://www.github.com/crushoverride007/product/material-dashboard-react
-* Copyright 2022 Matrix-Nexus (https://www.github.com/crushoverride007)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // @mui material components
 import { createTheme } from "@mui/material/styles";
 // import Fade from "@mui/material/Fade";
